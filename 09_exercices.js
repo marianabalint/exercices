952957132acebe74698f3abdc94cef7e91f0bdbb
Mariana-PC\Mariana <mary_prociuc@yahoo.com>
2020-09-21 18:53:09 +0300

    update

---
+++ 09_exercices.js
@@ -0,0 +1,42 @@
+// read and practice promises - get the examples from promises directory
+
+// 1. read and understand box model; 
+margin-Clears an area outside the border. The margin is transparent
+border-A border that goes around the padding and content
+padding-lears an area around the content. The padding is transparent
+content-he content of the box, where text and images appear
+
+div {
+    background-color: lightgrey;  content
+    width: 300px;
+    border: 15px solid green;
+    padding: 50px;
+    margin: 20px;
+  }
+
+// go over htmlelements -just read them and take a look
+
+// inspect how a site is made; 
+
+// go to bootsnipp.com and search for some examples; inspect how do they do it
+// also i've added the links_css file; go over links and check them;
+
+// play that game with frog - flex
+
+// check what's the difference between flex and flexbox grid
+Being one-dimensional, Flexbox only deals with either columns or rows. 
+This system works for smaller layouts, but cannot render complex displays such as text or document-centric
+properties that enable floats and columns.
+
+Grid has two-dimension layout capabilities which allow flexible widths as a unit of length.
+
+// search on google after css questions interview. put into a html file 50 of them
+
+// create a css flex box menu: https://stackoverflow.com/questions/50986565/visual-studio-target-framework-missing
+// i suggest to start with video 1 from "the net ninja"
+
+// think to a page layout and try to create. 
+
+
+
+
